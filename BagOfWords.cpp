a816cba6e6f3653da135be5488a02365a98ac522
Gerardo Moguel <andresmoguel05@hotmail.com>
2025-05-09 03:43:15 -0600

    Generated comments and basic structure
    Described the porpuse of each class and how the code will interact with eachs scipt

---
+++ BagOfWords.cpp
@@ -0,0 +1,30 @@
+/*Gerardo Moguel
+
+
+============================================================================================
+This class generates an individual hashmap of unique words used in each book,
+it also generates a "global hashmap" with the words all the books sent.
+
+    This hashmaps are used in the "word counter" algorithms.
+
+This process will be made serial and parallel, because i got curious if it can be paralelized,
+and how this affects the execution time.
+Thanks for reading.
+============================================================================================
+*/
+
+class BagOfWords{
+
+    /*
+============================================================================================
+    Reads the inputs delivered by the Python script
+    (It asks for the number of books and the location of each file)
+============================================================================================
+    */
+
+    //code where it receives the locations from the python script
+    //generates the hashmaps of each book
+    //generates the global hashmaps
+    //it calls the word counter algorithm to execute the rest of the code
+    
+};
