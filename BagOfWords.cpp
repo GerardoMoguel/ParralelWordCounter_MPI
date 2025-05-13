919fe1d06f8cf72689da6d2ad002db70e07c6ab6
Gerardo Moguel <andresmoguel05@hotmail.com>
2025-05-12 21:37:13 -0600

    finished doing the bad of words script
    now i need to do the parallel counting, accepting parameters from console.

--- BagOfWords.cpp
+++ BagOfWords.cpp
@@ -2,12 +2,8 @@
 
 
 ============================================================================================
-This class generates a unordered_map of every unique word used in the books.
-This hashmaps are used in the "word counter" algorithms.
-
-This process will be made serial and parallel, because i got curious if it can be paralelized,
-and how this affects the execution time.
-Thanks for reading.
+This class generates a map of every unique word used in the books.
+This maps are used in the "word counter" algorithms.
 ============================================================================================
 
 
@@ -19,7 +15,8 @@ Thanks for reading.
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <unordered_map>
+#include <vector>
+#include <map>
 using namespace std;
 
 class BagOfWords{
@@ -29,8 +26,7 @@ class BagOfWords{
 ============================================================================================
 */
 int num_books=0;
-unordered_map<string, int> wordCount;
-
+vector<map<string,int>> listOfMaps;
 
 /*
 ============================================================================================
@@ -39,37 +35,54 @@ unordered_map<string, int> wordCount;
 ============================================================================================
 */
 
-void processFile(const string& path) {
+void processFile(const string& path) { //won't modify the String, therefore is a constant, and "&" to avoid making copies
     ifstream file(path);
     string line;
-
+    map<string,int> global_map;
+    listOfMaps.push_back(global_map);
+    //tries to open the file
     if (!file.is_open()) {
         cout << "Could not open the file." << endl;
         return;
     }
+    getline(file, line); //reads the first line, to get the num of books.
+    int numBooks = stoi(line); 
+    //with the number of books, it creates n maps, to have individual bag of words for each book, 
+    //it also fills the "global" map with all the words
+    string bookPath; //variable to keep the paths Strings
+    for(int i=1;i<=numBooks;i++){
+        getline(file,bookPath);
+        map<string,int> map;
+        listOfMaps.push_back(map); //creates the map in the array
+        file.close(); //closes the last file
+        ifstream file(bookPath); //opens new file
 
-    while (getline(file, line)) {
-        istringstream stream(line);
-        string word;
+        if (!file.is_open()) {
+            cout << "Could not open the file." << endl;
+            return;
+        }
+        
+        //puts each unique word in the map i.
+        while (getline(file, line)) {
+            istringstream stream(line);
+            string word;
 
-        while (stream >> word) {
-            cout << "Word: " << word << endl;
+            while (stream >> word) {
+                if (listOfMaps[i].find(word) == listOfMaps[i].end()) { // If the word isn't in the map
+                    listOfMaps[i][word] = 0;  //it saves it with the value of 0.
+                }
+                if (listOfMaps[0].find(word) == listOfMaps[0].end()) { // find the word in the global map.
+                    listOfMaps[0][word] = 0;
+                }
+            }
         }
     }
 
     file.close();
 }
 
-int main() {
-    string path = "test.txt";
-    processFile(path);  // Call the void method
-    return 0;
-}
-
-    //Generates the hashmaps of each book
-
-    //Join the hashmaps together for global analisis
-
     //Executes the WordCounters.cpp script
+    //giving the array of maps as parameter.
+    
 
 };
