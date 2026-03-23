91777a6d990a32e9c69895701c8d1f4af61bcb43
Gerardo Moguel <andresmoguel05@hotmail.com>
2026-03-22 18:08:14 -0600

    more

--- Code/ParallelCounter.cpp
+++ Code/ParallelCounter.cpp
@@ -11,3 +11,32 @@
 using namespace std;
 using namespace std::chrono;
 
+class ParallelCounter{
+    public:
+    void parallelCount(int rank, int size, const string& fileName){ //we ask for rank and size, also the path for the book files (they're all in a txt file).
+        if(rank==0){ //the rank 0 will function as server
+            //it creates the Map
+            map<string, vector<int>> wordMap;
+
+            //list of book paths
+            
+            //reads the files
+
+            //sends each process the book they will analyze
+
+            //receives the responses and adds them to its map.
+
+        }
+        else{
+            //if youre not the server, you receive a book, and start analizing the word-counting.
+
+            //each word you analize, it sends the information to the server
+
+            //size of word
+
+            //word
+
+            //index of processes / book
+        }
+    }
+};
